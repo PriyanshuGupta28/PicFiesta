5550a11924e1a1036d5abe039ad28bcf8f789b2d
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-18 11:42:05 +0530

    Update Illustrations.js

--- src/Pages/Illustrations/Illustrations.js
+++ src/Pages/Illustrations/Illustrations.js
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from "react";
-import SingleImage from "../../Components/SingleImage/SingleImage";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import axios from "axios";
+import React, { Suspense, useEffect, useState } from "react";
 import MyHero from "../../Components/MyHero/MyHero";
 import "./Illustrations.css";
 import NavLinks from "../../Components/NavLinks/NavLinks";
-import Trasition from "../../Trasition/Trasition";
+import useAxios from "../../CustomHooks/useAxios";
+import Loader from "../../Components/Loader/Loader";
+import { pixabayKey } from "../../Utility/Utils/utilsFunctions";
+
+const ReactMasonry = React.lazy(() =>
+  import("../../Components/ReactMasonry/ReactMasonry")
+);
 
 const Illustrations = () => {
-  const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-
-  const [loading, setLoading] = useState(true);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `https://pixabay.com/api/?key=41123393-d488d28859f6869a5072a3240&q=iphone+Wallpapers&image_type=illustration&editors_choice=true$&per_page=20&page=${page}`
-      );
-      setData((prevData) => [...prevData, ...response?.data?.hits]);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [page]);
+  const { data, loading, error, response } = useAxios(
+    `https://pixabay.com/api/?key=${pixabayKey}&q=iphone+Wallpapers&image_type=illustration&editors_choice=true$&per_page=20&page=${page}`
+  );
 
   const handelInfiniteScroll = async () => {
     try {
@@ -34,7 +22,6 @@ const Illustrations = () => {
         window.innerHeight + document.documentElement.scrollTop + 1 >=
         document.documentElement.scrollHeight
       ) {
-        setLoading(true);
         setPage((prev) => prev + 1);
       }
     } catch (error) {
@@ -46,25 +33,25 @@ const Illustrations = () => {
     window.addEventListener("scroll", handelInfiniteScroll);
     return () => window.removeEventListener("scroll", handelInfiniteScroll);
   }, []);
+
+  const heroDetails = {
+    title: "Free illustrations for download & inspiration",
+    description:
+      "Over 630,000+ beautiful free illustrations shared by our talented community.",
+    images: [data[11]?.largeImageURL],
+  };
   return (
     <>
-      <MyHero />
-      <NavLinks />
-      <div className="illustrations-root">
-        <div className="illustrations-masonry">
-          <ResponsiveMasonry
-            columnsCountBreakPoints={{ 350: 1, 750: 3, 900: 4 }}
-          >
-            <Masonry columnsCount={4} gutter={20}>
-              {data.map((e) => (
-                <SingleImage key={e?.id} data={e} />
-              ))}
-            </Masonry>
-          </ResponsiveMasonry>
-        </div>
-      </div>
+      <Suspense fallback={<Loader />}>
+        <ReactMasonry
+          key={"Illustrations"}
+          data={data}
+          loading={loading}
+          error={error}
+        />
+      </Suspense>
     </>
   );
 };
 
-export default Trasition(Illustrations);
+export default Illustrations;
