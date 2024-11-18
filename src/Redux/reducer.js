73ea85850cebd4751061e9f837d9a4bb0864164e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-18 11:42:35 +0530

    Update reducer.js

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import HomeReducer from "./HomeReducer/HomeReducer";
 import NavLinksReducer from "./NavLinksReducer/NavLinksReducer";
+import SearchReducer from "./SearchReducer/SearchReducer";
 
 export const reducer = combineReducers({
   home: HomeReducer,
   navLinks: NavLinksReducer,
+  searchQuery: SearchReducer,
 });
