73a6b79f04312a9100eed28ffb307b5700317da3
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-18 11:41:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { FloatButton } from "antd";
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes/AllRoutes";
+import AppFooter from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       <AllRoutes />
-    </div>
+      <AppFooter />
+      <FloatButton.BackTop />
+    </>
   );
 }
 
