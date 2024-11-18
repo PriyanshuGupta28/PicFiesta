7f6c595ea2af79bea23b663c21d94e9aab608516
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-18 11:42:18 +0530

    Update Signup.js

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,38 +1,8 @@
-import {
-  SignInButton,
-  SignOutButton,
-  SignUp,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/clerk-react";
-import auth from "../../Components/Firebase/Firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import { SignUp } from "@clerk/clerk-react";
+import React from "react";
 import { Flex } from "antd";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignup = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password).then(
-        (userCredential) => {
-          // Signed up
-          const user = userCredential.user;
-          console.log(user, "handleSignup");
-          // ...
-        }
-      );
-    } catch (error) {
-      const errorCode = error?.code;
-      const errorMessage = error?.message;
-      console.log(errorMessage, errorCode, "handleSignup");
-    }
-  };
-
   return (
     <div>
       <Flex justify="center" align="center">
