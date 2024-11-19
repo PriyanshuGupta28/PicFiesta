446a09c58973fc4ecbf19be4dba0a56792556605
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-20 02:13:37 +0530

    Update SearchReducer.js

--- src/Redux/SearchReducer/SearchReducer.js
+++ src/Redux/SearchReducer/SearchReducer.js
@@ -1,16 +1,31 @@
-import { SEARCHQUERY } from "../../Utility/ReduxConstants/SearchConstants/SearchConstants";
+import {
+  FILTERQUERY,
+  SEARCHQUERY,
+} from "../../Utility/ReduxConstants/SearchConstants/SearchConstants";
 
 const initState = {
   searchQuery: "",
+  filterQuery: {
+    orientation: "all",
+    imageType: "all",
+    category: "",
+    order: "popular",
+    editorsChoice: false,
+  },
 };
 
 export default (state = initState, action) => {
   switch (action.type) {
     case SEARCHQUERY:
-      console.log(action.payload, "GETCURRENTLOCATION");
       return {
         ...state,
         searchQuery: action.payload,
+      };
+    case FILTERQUERY:
+      console.log(action.payload, "filterQuery");
+      return {
+        ...state,
+        filterQuery: { ...state.filterQuery, ...action.payload },
       };
     default:
       return state;
