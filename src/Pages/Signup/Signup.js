5db785ad8e6ebae879a102ebdac73c2753956fee
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-19 11:28:28 +0530

    Update Signup.js

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,15 +1,7 @@
-import { SignUp } from "@clerk/clerk-react";
 import React from "react";
-import { Flex } from "antd";
 
 const Signup = () => {
-  return (
-    <div>
-      <Flex justify="center" align="center">
-        <SignUp />
-      </Flex>
-    </div>
-  );
+  return <div>Signup</div>;
 };
 
 export default Signup;
