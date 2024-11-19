db51588b68dbec3c3bd24b275d36f78589e10bf0
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-19 11:28:21 +0530

    Update Login.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,7 @@
 import React from "react";
-import "./Login.scss";
-
-import { SignIn } from "@clerk/clerk-react";
 
 const Login = () => {
-  return (
-    <div className="login-root">
-      <div className="login-wraper">
-        <SignIn signUpUrl="/signup" />
-      </div>
-    </div>
-  );
+  return <div>Login</div>;
 };
 
 export default Login;
