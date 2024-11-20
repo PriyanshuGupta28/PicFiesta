96a7fe8e108adf86c2e0eb92643f9e791f5eea4b
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-20 15:30:38 +0530

    Update Illustrations.js

--- src/Pages/Illustrations/Illustrations.js
+++ src/Pages/Illustrations/Illustrations.js
@@ -5,6 +5,7 @@ import NavLinks from "../../Components/NavLinks/NavLinks";
 import useAxios from "../../CustomHooks/useAxios";
 import Loader from "../../Components/Loader/Loader";
 import { pixabayKey } from "../../Utility/Utils/utilsFunctions";
+import Seo from "../../Components/Seo/Seo";
 
 const ReactMasonry = React.lazy(() =>
   import("../../Components/ReactMasonry/ReactMasonry")
@@ -35,6 +36,16 @@ const Illustrations = () => {
   }, []);
   return (
     <>
+      <Seo
+        title="Picfiesta | Illustrations - Creative Artwork"
+        description="Discover a variety of creative illustrations on Picfiesta. Perfect for design inspiration and projects."
+        keywords="illustrations, creative artwork, Picfiesta illustrations, graphic design"
+        canonicalUrl="https://www.Picfiesta.com/illustrations"
+        ogTitle="Illustrations | Picfiesta"
+        ogDescription="Browse creative illustrations and graphic artwork on Picfiesta."
+        ogImage="https://www.Picfiesta.com/assets/og-illustrations.png"
+      />
+
       <Suspense fallback={<Loader />}>
         <ReactMasonry
           key={"Illustrations"}
